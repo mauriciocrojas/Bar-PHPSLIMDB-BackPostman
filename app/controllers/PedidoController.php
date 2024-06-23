3a44b2231ad202bc0db5120dbd5057f5df73577c
mauriciocrojas <mauriciocc.rojas@gmail.com>
2024-06-23 02:27:58 -0300

    creacionPedidoProducto

--- app/controllers/PedidoController.php
+++ app/controllers/PedidoController.php
@@ -9,18 +9,53 @@ class PedidoController extends Pedido implements IApiUsable
   {
     $parametros = $request->getParsedBody();
 
-    $idmesa = $parametros['idmesa'];
-    //$idproducto = $parametros['idproducto'];
-    $nombrecliente = $parametros['nombrecliente'];
+    $parametrosOk = false;
+    $listaOk = false;
 
-    // Creamos el pedido
-    $pedido = new Pedido();
-    $pedido->idmesa = $idmesa;
-    //$pedido->idproducto = $idproducto;
-    $pedido->nombrecliente = $nombrecliente;
-    $pedido->crearPedido();
+    $productosJson = $parametros['productos'] ?? null;
+    $productos = json_decode($productosJson, true); // Convierto a array asociativo
 
-    $payload = json_encode(array("mensaje" => "Pedido creado con exito"));
+    if (isset($parametros['idmesa']) && isset($parametros['nombrecliente'])) {
+      $parametrosOk = true;
+    } else {
+      echo "Los parámetros del pedido no fueron seteados correctamente";
+    }
+
+    if (isset($productosJson)) {
+      foreach ($productos as $producto) {
+        if (isset($producto['idproducto']) && isset($producto['cantidad'])) {
+          $listaOk = true;
+        } else {
+          $listaOk = false;
+          echo "La lista de productos no fue seteada correctamente";
+          break;
+        }
+      }
+    } else {
+      echo "La lista de productos no fue seteada";
+      $listaOk = false;
+    }
+
+    if ($parametrosOk && $listaOk) {
+      $idmesa = $parametros['idmesa'];
+      $nombrecliente = $parametros['nombrecliente'];
+
+
+      // Creamos el pedido
+      $pedido = new Pedido();
+      $pedido->idmesa = $idmesa;
+      $pedido->nombrecliente = $nombrecliente;
+      $pedido->productos = $productos;
+      $pedido->crearPedido();
+
+      $payload = json_encode(array("mensaje" => "Pedido creado con exito"));
+
+      $response->getBody()->write($payload);
+      return $response
+        ->withHeader('Content-Type', 'application/json');
+    } else {
+      $payload = json_encode(array("mensaje" => "El pedido no fue creado"));
+    }
 
     $response->getBody()->write($payload);
     return $response
--- app/models/Pedido.php
+++ app/models/Pedido.php
@@ -10,25 +10,42 @@ class Pedido
     public $nombreimagen;
     public $tiempoestimado;
 
+    protected $productos = [];
+
+
 
 
     public function crearPedido()
     {
+
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
         $consulta = $objAccesoDatos->prepararConsulta("INSERT INTO pedido (idmesa, nombrecliente) VALUES (:idmesa, :nombrecliente)");
         $consulta->bindValue(':idmesa', $this->idmesa, PDO::PARAM_INT);
-        //$consulta->bindValue(':idproducto', $this->idproducto, PDO::PARAM_INT);
         $consulta->bindValue(':nombrecliente', $this->nombrecliente, PDO::PARAM_STR);
         $consulta->execute();
+
+        $consultaPedidoProducto = $objAccesoDatos->prepararConsulta("INSERT INTO pedidoproducto (idpedido, idproducto, cantidad) VALUES (:idpedido, :idproducto, :cantidad)");
+
+        $idpedido = $objAccesoDatos->obtenerUltimoId();
+
+        foreach ($this->productos as $producto) {
+            $consultaPedidoProducto->bindValue(':idpedido', $idpedido, PDO::PARAM_INT);
+            $consultaPedidoProducto->bindValue(':idproducto', $producto['idproducto'], PDO::PARAM_INT);
+            $consultaPedidoProducto->bindValue(':cantidad', $producto['cantidad'], PDO::PARAM_INT);
+            $consultaPedidoProducto->execute();
+        }
 
         return $objAccesoDatos->obtenerUltimoId();
     }
 
 
+
+
     public static function obtenerTodos()
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("SELECT idpedido, idmesa, estado, nombrecliente, tiempoestimado FROM pedido");
+        $consulta = $objAccesoDatos->prepararConsulta("SELECT pe.idpedido, pe.idmesa, pe.estado, pe.nombrecliente, pe.tiempoestimado, pp.idproducto, pp.cantidad FROM pedido pe inner join pedidoproducto pp 
+        on pe.idpedido = pp.idpedido");
         $consulta->execute();
 
         return $consulta->fetchAll(PDO::FETCH_CLASS, 'Pedido');
@@ -37,11 +54,13 @@ class Pedido
     public static function obtenerPedido($id)
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("SELECT idpedido, idmesa, estado, nombrecliente, tiempoestimado FROM pedido WHERE idpedido = :idpedido");
+        $consulta = $objAccesoDatos->prepararConsulta("SELECT pe.idpedido, pe.idmesa, pe.estado, pe.nombrecliente, pe.tiempoestimado, pp.idproducto, pp.cantidad FROM pedido pe inner join pedidoproducto pp 
+        on pe.idpedido = pp.idpedido
+        WHERE pp.idpedido = :idpedido");
         $consulta->bindValue(':idpedido', $id, PDO::PARAM_STR);
         $consulta->execute();
 
-        return $consulta->fetchObject('Pedido');
+        return $consulta->fetchAll(PDO::FETCH_CLASS, 'Pedido');
     }
 
     public static function modificarPedido($id, $estado)
